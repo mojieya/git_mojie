93721994063eb25de343f829fed89fa3cdf64b1a
mojieya <45915816+mojieya@users.noreply.github.com>
2018-12-16 22:15:00 +0800

    Update Test.java
    在中央仓库修改Test

--- mojie/src/mojie/Test.java
+++ mojie/src/mojie/Test.java
@@ -2,6 +2,9 @@ package mojie;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("ħ�ܡ�����");
+		System.out.println("Ä§½Ü¡£¡£¡£");
+	}
+	public void zl(){
+		System.out.println("66666");
 	}
 }
